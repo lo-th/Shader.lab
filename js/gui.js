9c6f5dc16f4b421a52e927f415cdb2fa686eedf9
lo-th <3dflashlo@gmail.com>
2016-08-04 22:46:13 +0200

    up

--- js/gui.js
+++ js/gui.js
@@ -11,7 +11,7 @@ var gui = ( function () {
         basic: function(){
 
             ui.add('fps',  {});
-            ui.add('title',  { name:'Quality' });
+            ui.add('title',  { name:'Pixel Ratio' });
             ui.add('button',  {width:54, sa:2, sb:52, name:0.125}).onChange( function(){view.setQuality(0.125)} );
             ui.add('button',  {width:54, sa:2, sb:52, name:0.25}).onChange( function(){view.setQuality(0.25)} );
             ui.add('button',  {width:54, sa:2, sb:52, name:0.5}).onChange( function(){view.setQuality(0.5)} );
